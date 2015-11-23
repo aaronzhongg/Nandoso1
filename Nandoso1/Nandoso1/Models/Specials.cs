ea2a9c1caf80ff4004aa9eecfed92db0fd1ec0d4
azho472 <aaron_zhong2b@hotmail.com>
2015-11-23 21:15:14 +1300

    Add Specials model and controller, added bootstrap nuget and created index.html

---
+++ Nandoso1/Nandoso1/Models/Specials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nandoso1.Models
+{
+    public class Specials
+    {
+        public int ID { get; set; }
+        public string ItemName { get; set; }
+        public string NormalPrice { get; set; }
+        public string SpecialPrice { get; set; }
+    }
+}
