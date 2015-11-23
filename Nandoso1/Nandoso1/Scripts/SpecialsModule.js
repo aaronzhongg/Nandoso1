dbe3c7fa80b8d4e5a2fd262bf44d8de01055859d
azho472 <aaron_zhong2b@hotmail.com>
2015-11-24 09:03:57 +1300

    Added Swagger, StudentModel.js and updated webconfig to use db - First publish to azure

---
+++ Nandoso1/Nandoso1/Scripts/SpecialsModule.js
@@ -0,0 +1,21 @@
+﻿// We've sepearated out all the functions related to making the AJAX calls to the API
+// Just keeps things tidy, allows us to keep some things private
+var SpecialsModule = (function () {
+
+    // Return anything that you want to expose outside the closure
+    return {
+        getStudents: function (callback) {
+
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://localhost:50124/api/Specials",
+                success: function (data) {
+                    console.log(data);
+                    callback(data);
+                }
+            });
+
+        }
+    };
+}());
