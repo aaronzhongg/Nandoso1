cde77a8d5305412315e26155ca86de0ebcda0c57
azho472 <aaron_zhong2b@hotmail.com>
2015-11-26 12:54:07 +1300

    update tbl to remove items when logged out

--- Nandoso1/Scripts/facebooklogin.js
+++ Nandoso1/Scripts/facebooklogin.js
@@ -14,11 +14,13 @@ function statusChangeCallback(response) {
         // The person is logged into Facebook, but not your app.
         document.getElementById('status').innerHTML = 'Please log ' +
           'into this app.';
+        $("#tblspecialscontent").html("");
     } else {
         // The person is not logged into Facebook, so we're not sure if
         // they are logged into this app or not.
         document.getElementById('status').innerHTML = 'Please log ' +
           'into Facebook.';
+        $("#tblspecialscontent").html("");
     }
 }
 
