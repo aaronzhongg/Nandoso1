006d13327e80b11f4a6ee1ce6b2e243412c45431
azho472 <aaron_zhong2b@hotmail.com>
2015-11-26 13:22:47 +1300

    update fb log in msgs

--- Nandoso1/Scripts/facebooklogin.js
+++ Nandoso1/Scripts/facebooklogin.js
@@ -9,18 +9,20 @@ function statusChangeCallback(response) {
     if (response.status === 'connected') {
         // Logged into your app and Facebook.
         document.getElementById('logged').style.visibility = "hidden";
-        loadSpecials();
         testAPI();
+        loadSpecials();
     } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
         document.getElementById('status').innerHTML = 'Please log ' +
           'into this app.';
+        document.getElementById('logged').style.visibility = "visible"
         $("#tblspecialscontent").html("");
     } else {
         // The person is not logged into Facebook, so we're not sure if
         // they are logged into this app or not.
         document.getElementById('status').innerHTML = 'Please log ' +
           'into Facebook.';
+        document.getElementById('logged').style.visibility = "visible"
         $("#tblspecialscontent").html("");
     }
 }
